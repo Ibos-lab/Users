5ad4ff6fcf4e69455c16e555909c3ccef7a7fc1c
Camila Losada <camilosadaa@hotmail.com>
2024-05-24 18:31:58 +0200

    wip: pca by session

--- losadac/pca/run_pca_by_session.py
+++ losadac/pca/run_pca_by_session.py
@@ -15,6 +15,14 @@ from typing import Dict, List
 import pca_tools
 from scipy import stats
 from ephysvibe.task import task_constants
+from sklearn.preprocessing import StandardScaler
+from scipy.ndimage.filters import gaussian_filter1d
+import scipy as sp
+from matplotlib import animation
+from IPython.display import HTML
+import matplotlib
+
+matplotlib.rcParams["animation.embed_limit"] = 2**128
 
 seed = 2024
 
@@ -43,6 +51,15 @@ def from_python_hdf5(load_path: Path):
             data.append(dataset)
     f.close()
     return data
+
+
+def z_score(X, with_std=False):
+    # X: ndarray, shape (n_features, n_samples)
+    # maxx = np.max(X,axis=1).reshape(-1,1)
+    # X=X/ maxx
+    ss = StandardScaler(with_mean=True, with_std=with_std)
+    Xz = ss.fit_transform(X.T).T
+    return Xz, ss  # ,maxx
 
 
 def get_fr_samples(sp, sample_id, start, end, samples, min_trials):
@@ -171,7 +188,7 @@ def get_neuron_sample_test1_fr(
     )[:, time_before : time_before + end_test + 400]
     sp = np.concatenate((avg_sample_on, avg_test1_on), axis=1)[:, idx_start:idx_end]
     # Check fr
-    ms_fr = np.nanmean(sp) * 1000 > 5
+    ms_fr = np.nanmean(sp) * 1000 > 1
     if not ms_fr:
         return {"fr": None}
 
@@ -219,7 +236,7 @@ time_before = 500
 start = -200
 end_test = n_test * 450 + 200
 end = 450 + 400 + 400 + end_test
-win = 50
+win = 200
 # Define epochs
 part1 = 200 + 450 + 400
 test1_st = part1 + 400
@@ -331,7 +348,7 @@ for session in all_sessions:
             s55.append(fr["55"])
     neurons_fr = [{"0": s0, "11": s11, "15": s15, "51": s51, "55": s55}]
 
-    if len(s55) < 4:
+    if len(s55) < 6:
         continue
     basepath = "./"  #'/envau/work/invibe/USERS/IBOS/data/Riesling/TSCM/OpenEphys/pca/data/'+area +'/'
     to_python_hdf5(
@@ -366,22 +383,25 @@ for session in all_sessions:
     )
     neurons_fr = neurons_fr_read[0]
 
-    mean11 = np.mean(neurons_fr["11"], axis=1)
-    mean15 = np.mean(neurons_fr["15"], axis=1)
-    mean51 = np.mean(neurons_fr["51"], axis=1)
-    mean55 = np.mean(neurons_fr["55"], axis=1)
-    mean0 = np.mean(neurons_fr["0"], axis=1)
-
     n_comp = len(neurons_fr["11"])
-    model, pc_s = pca_tools.compute_pca(
-        np.concatenate((mean0, mean11, mean15, mean51, mean55), axis=1), n_comp=n_comp
+    all_mean = np.concatenate(
+        (
+            neurons_fr["0mean"],
+            neurons_fr["11mean"],
+            neurons_fr["15mean"],
+            neurons_fr["51mean"],
+            neurons_fr["55mean"],
+        ),
+        axis=1,
     )
+    all_mean, ss = z_score(all_mean, with_std=False)
+    model, pc_s = pca_tools.compute_pca(all_mean, n_comp=n_comp)
+
     fig, ax = plt.subplots(figsize=(5, 5))
     pca_tools.plot_explained_var(model, figsize=(5, 5), area=area, fig=fig, ax=ax)
     if save:
         figname = area + session + "explained_var." + save_format
         fig.savefig(savepath + figname, format=save_format, bbox_inches="tight")
-
     reshape_pc_s = pc_s.reshape(n_comp, -1, end - start)
     mean_pc_s = np.mean(reshape_pc_s[:, 1:, :], axis=1)
     fig, ax = plt.subplots(2, 3, figsize=(17, 10))
@@ -407,6 +427,7 @@ for session in all_sessions:
         ax=ax,
         idot=-1,
     )
+
     if save:
         figname = area + session + "_pcs_neutral_sample." + save_format
         fig.savefig(savepath + figname, format=save_format, bbox_inches="tight")
@@ -422,3 +443,360 @@ for session in all_sessions:
     if save:
         figname = area + session + "_pcs_sample_mean." + save_format
         fig.savefig(savepath + figname, format=save_format, bbox_inches="tight")
+
+    ## Transform trials
+    ssneurons_fr = {}
+    for i_s in ("0", "11", "15", "51", "55"):
+        emptyarr = np.zeros(neurons_fr[i_s].shape)
+        for itr in range(neurons_fr[i_s].shape[1]):
+            emptyarr[:, itr, :] = ss.transform((neurons_fr[i_s][:, itr]).T).T
+        ssneurons_fr[i_s] = emptyarr
+
+    proj_sam = {}
+    for i_s in ("0", "11", "15", "51", "55"):
+
+        tr_proj = model.transform(ssneurons_fr[i_s].reshape(n_comp, -1).T).T
+        tr_proj = tr_proj.reshape(n_comp, -1, ssneurons_fr[i_s].shape[-1])
+        proj_sam[i_s] = tr_proj
+
+    color1 = [
+        "#0A4580",
+        "#0F8792",
+        "#13A272",
+        "#18B13C",
+        "#3EBF1E",
+        "#8ECD25",
+        "#DAD02C",
+        "#E69034",
+        "#EC3E3B",
+        "#F14297",
+        "#F54AF2",
+        "#A953F9",
+        "#5E5CFD",
+        "#65B0FF",
+    ]
+    color2 = [
+        "#81A2C3",
+        "#84C6CC",
+        "#85D4BA",
+        "#87DC9B",
+        "#9CE38A",
+        "#C8EB8E",
+        "#F1EC91",
+        "#F7C895",
+        "#FA9A98",
+        "F14297",
+        "#FEB4D8",
+        "#DFBAFF",
+        "#BFBEFF",
+        "#C1E1FF",
+    ]
+    sigma = 3
+    darkcolor = {
+        "0": "k",
+        "11": "firebrick",  # o1_c1
+        "15": "teal",  # o1_c5
+        "51": "tomato",  # o5_c1
+        "55": "c",  # "lightseagreen",  #
+    }
+
+    part1 = 200 + 450 + 400
+    f, ax = plt.subplots(3, 2, figsize=(18, 10))
+    for i_sample in ["0", "11", "15", "51", "55"]:
+        trialspc = proj_sam[i_sample]
+
+        for itr in range(0, trialspc.shape[1], 10):
+            pcs = trialspc[:, itr]
+            irow, icol = 0, 0
+            for i_c in range(6):
+                if irow == 3:
+                    irow, icol = 0, 1
+                ax[irow, icol].plot(
+                    range(start, end)[:part1],
+                    pcs[i_c, :part1],
+                    color=task_constants.PALETTE_B1[i_sample],
+                    label=i_sample,
+                    alpha=0.1,
+                )
+                ax[irow, icol].plot(
+                    range(start, end)[part1:],
+                    pcs[i_c, part1:],
+                    color=task_constants.PALETTE_B1[i_sample],
+                    label=i_sample,
+                    alpha=0.1,
+                )
+                ax[irow, icol].set(xlabel="Time(ms)", ylabel="PC" + str(i_c + 1))
+                irow = irow + 1
+    for i_sample in ["0", "11", "15", "51", "55"]:
+        trialspc = proj_sam[i_sample]
+        irow, icol = 0, 0
+        for i_c in range(6):
+            if irow == 3:
+                irow, icol = 0, 1
+            ax[irow, icol].plot(
+                range(start, end)[:part1],
+                np.mean(trialspc[i_c, :, :part1], axis=0),
+                color=darkcolor[i_sample],
+                label=i_sample,
+                alpha=1,
+            )
+            ax[irow, icol].plot(
+                range(start, end)[part1:],
+                np.mean(trialspc[i_c, :, part1:], axis=0),
+                color=darkcolor[i_sample],
+                label=i_sample,
+                alpha=1,
+            )
+            irow = irow + 1
+    if save:
+        figname = area + session + "_pcs_time_trial." + save_format
+        fig.savefig(savepath + figname, format=save_format, bbox_inches="tight")
+
+    part1 = 200 + 450 + 400
+    f, ax = plt.subplots(3, 2, figsize=(18, 10))
+    for i_sample in ["0", "11", "15", "51", "55"]:
+        trialspc = proj_sam[i_sample]
+        irow, icol = 0, 0
+        for i_c in range(6):
+            if irow == 3:
+                irow, icol = 0, 1
+            h = np.std(trialspc[i_c, :, :part1], axis=0)
+            ax[irow, icol].plot(
+                range(start, end)[:part1],
+                np.mean(trialspc[i_c, :, :part1], axis=0),
+                color=darkcolor[i_sample],
+                label=i_sample,
+                alpha=1,
+            )
+            ax[irow, icol].fill_between(
+                range(start, end)[:part1],
+                np.mean(trialspc[i_c, :, :part1], axis=0) + h,
+                np.mean(trialspc[i_c, :, :part1], axis=0) - h,
+                color=task_constants.PALETTE_B1[i_sample],
+                alpha=0.1,
+            )
+            ## part 2
+            h = np.std(trialspc[i_c, :, part1:], axis=0)
+            ax[irow, icol].plot(
+                range(start, end)[part1:],
+                np.mean(trialspc[i_c, :, part1:], axis=0),
+                color=darkcolor[i_sample],
+                label=i_sample,
+                alpha=1,
+            )
+            ax[irow, icol].fill_between(
+                range(start, end)[part1:],
+                np.mean(trialspc[i_c, :, part1:], axis=0) + h,
+                np.mean(trialspc[i_c, :, part1:], axis=0) - h,
+                color=task_constants.PALETTE_B1[i_sample],
+                alpha=0.1,
+            )
+            ax[irow, icol].set(xlabel="Time(ms)", ylabel="PC" + str(i_c + 1))
+            irow = irow + 1
+    if save:
+        figname = area + session + "_pcs_time_std." + save_format
+        fig.savefig(savepath + figname, format=save_format, bbox_inches="tight")
+
+    # Animation
+    color_epo = ["#0A4580", "#18B13C", "#DAD02C", "#F14297", "#5E5CFD", "#65B0FF"]
+
+    for i_sample in ["0", "11", "15", "51", "55"]:
+        trialspc = proj_sam[i_sample]
+        # apply some smoothing to the trajectories
+        sigma = 3  # smoothing amount
+
+        # mean_pc_s= np.mean(reshape_pc_s[:,1:,:],axis=1)
+        g_mean_pc = trialspc.copy()
+
+        for c in range(g_mean_pc.shape[0]):
+            for i_tr in range(g_mean_pc.shape[1]):
+                g_mean_pc[c, i_tr, :] = gaussian_filter1d(
+                    g_mean_pc[c, i_tr, :], sigma=sigma
+                )
+
+        # g_mean_pc=g_mean_pc.reshape(n_comp,-1,end-start)
+
+        x1 = g_mean_pc[:, :, :-200].copy()
+        x1[:, :, 200 + 450 + 400 :] = np.nan
+        x2 = g_mean_pc[:, :, 200:].copy()
+        x2[:, :, : 450 + 400] = np.nan
+
+        time = np.arange(0, x2.shape[-1]) - 200
+
+        # utility function to clean up and label the axes
+        def style_ax(ax, xlabel, ylabel, i_sample):
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+            ax.set_title("%s - sample %s" % (area.upper(), i_sample))
+
+        mean_pc_s = np.mean(reshape_pc_s[:, 1:, :], axis=1)
+        for c in range(mean_pc_s.shape[0]):
+            mean_pc_s[c, :] = gaussian_filter1d(mean_pc_s[c, :], sigma=sigma)
+
+        for component_x in np.arange(0, 2):
+            for component_y in np.arange(0 + component_x, 2):
+                if component_x == component_y:
+                    continue
+                # create the figure
+                fig = plt.figure(figsize=[9, 9])
+                ax = fig.add_subplot()  # 1, 1, projection='3d')
+                plt.close()
+
+                # annotate with stimulus and time information
+                text = ax.text(
+                    0.05,
+                    0.95,
+                    "Fixation \nt = %d ms" % (time[0]),
+                    fontsize=14,
+                    transform=ax.transAxes,
+                    va="top",
+                )
+
+                def animate(i):
+                    # pick the components corresponding to the x, y, and z axes
+                    # component_x = 1
+                    # component_y = 2
+                    ax.clear()  # clear up trajectories from previous iteration
+                    xlabel = "PC " + str(component_x + 1)
+                    ylabel = "PC " + str(component_y + 1)
+                    style_ax(ax, xlabel, ylabel, i_sample)
+                    j = 0
+                    if i > 200:
+                        j = i - 200
+
+                    # plot avg
+                    color = task_constants.PALETTE_B1[i_sample]
+                    ax.plot(
+                        mean_pc_s[component_x, : 450 + 400],
+                        mean_pc_s[component_y, : 450 + 400],
+                        color,
+                    )
+                    ax.plot(
+                        mean_pc_s[component_x, 200 + 450 + 400 :],
+                        mean_pc_s[component_y, 200 + 450 + 400 :],
+                        color,
+                        linestyle="--",
+                    )
+
+                    for ic, itr in enumerate(range(0, g_mean_pc.shape[1], 10)):
+
+                        # plot 1st part
+                        x11 = x1[component_x, itr, 0:i]
+                        y11 = x1[component_y, itr, 0:i]
+                        ax.plot(x11[j:i], y11[j:i], color1[ic])
+                        # plot 2nd part
+                        x22 = x2[component_x, itr, 0:i]
+                        y22 = x2[component_y, itr, 0:i]
+                        ax.plot(x22[j:i], y22[j:i], color2[ic])
+
+                        xlim = [x1[component_x], x2[component_x]]
+                        ylim = [x1[component_y], x2[component_y]]
+                        ax.set_xlim((np.nanmin(xlim) - 0.005, np.nanmax(xlim) + 0.005))
+                        ax.set_ylim((np.nanmin(ylim) - 0.005, np.nanmax(ylim) + 0.005))
+                        # update stimulus and time annotation
+                        if i < 200:
+                            # stimdot.set_data(10, 14)
+                            if itr == 0:
+                                ax.text(
+                                    0.05,
+                                    0.95,
+                                    "Fixation \nt = %d ms" % (time[i]),
+                                    fontsize=14,
+                                    transform=ax.transAxes,
+                                    va="top",
+                                )
+                        elif np.logical_and(i > 200, i < 200 + 450):
+                            # stimdot.set_data(10, 14)
+                            ax.scatter(x11[200], y11[200], c=color_epo[0])
+                            if itr == 0:
+                                ax.text(
+                                    0.05,
+                                    0.95,
+                                    "Sample ON \nt = %d ms" % (time[i]),
+                                    fontsize=14,
+                                    transform=ax.transAxes,
+                                    va="top",
+                                )
+                        elif np.logical_and(i > 200 + 450, i < 200 + 450 + 600):
+                            # stimdot.set_data(10, 14)
+                            ax.scatter(x11[200], y11[200], c=color_epo[0])
+                            ax.scatter(x11[200 + 450], y11[200 + 450], c=color_epo[1])
+                            if itr == 0:
+                                ax.text(
+                                    0.05,
+                                    0.95,
+                                    "Delay \nt = %d ms" % (time[i]),
+                                    fontsize=14,
+                                    transform=ax.transAxes,
+                                    va="top",
+                                )
+                        elif np.logical_and(
+                            i > 200 + 450 + 600, i < 200 + 450 + 600 + 450
+                        ):
+                            # stimdot.set_data(10, 14)
+                            ax.scatter(x11[200], y11[200], c=color_epo[0])
+                            ax.scatter(x11[200 + 450], y11[200 + 450], c=color_epo[1])
+                            ax.scatter(
+                                x22[200 + 450 + 600],
+                                y22[200 + 450 + 600],
+                                c=color_epo[2],
+                            )
+                            if itr == 0:
+                                ax.text(
+                                    0.05,
+                                    0.95,
+                                    "Test ON \nt = %d ms" % (time[i]),
+                                    fontsize=14,
+                                    transform=ax.transAxes,
+                                    va="top",
+                                )
+                        else:
+                            # stimdot.set_data([], [])
+                            ax.scatter(x11[200], y11[200], c=color_epo[0])
+                            ax.scatter(x11[200 + 450], y11[200 + 450], c=color_epo[1])
+                            ax.scatter(
+                                x22[200 + 450 + 600],
+                                y22[200 + 450 + 600],
+                                c=color_epo[2],
+                            )
+                            ax.scatter(
+                                x22[200 + 450 + 600 + 450],
+                                y22[200 + 450 + 600 + 450],
+                                c=color_epo[3],
+                            )
+                            if itr == 0:
+                                ax.text(
+                                    0.05,
+                                    0.95,
+                                    "Test OFF \nt = %d ms" % (time[i]),
+                                    fontsize=14,
+                                    transform=ax.transAxes,
+                                    va="top",
+                                )
+                    return []
+
+                anim = animation.FuncAnimation(
+                    fig,
+                    animate,
+                    frames=range(1, x1.shape[-1], 2),
+                    interval=50,
+                    blit=True,
+                )
+
+                #
+                if save:
+                    figname = (
+                        area
+                        + "_"
+                        + i_sample
+                        + "_pc"
+                        + str(component_x + 1)
+                        + "_pc"
+                        + str(component_y + 1)
+                        + "_trials_animation.gif"
+                    )
+                    anim.save(figname, writer="Pillow", fps=30)
+                else:
+                    HTML(anim.to_jshtml(default_mode="once"))
