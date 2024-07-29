2064d10f9159938fea4fc02c633cedb7ac166cb6
camila <camilosadaa@hotmail.com>
2024-07-29 11:48:04 +0200

    wip

--- losadac/conectivity/prep_neurons_corr.py
+++ losadac/conectivity/prep_neurons_corr.py
@@ -51,9 +51,76 @@ def get_neu_align(path, params, sp_sample=False):
     return neu
 
 
+def get_fr(neu, inout, sample, st, end, win=100):
+    res = {}
+    mask = getattr(neu, "mask_" + inout)
+    sp = getattr(neu, "sample_on_" + inout)
+    fr = firing_rate.moving_average(data=sp, win=win, step=1)[:, st:end]
+    sample_id = neu.sample_id[mask]
+    fr_samples = select_trials.get_sp_by_sample(fr, sample_id, [sample])
+    frs = fr_samples[str(sample)]
+    if frs.shape[0] < 40:
+        return None
+    nzeros = np.sum(np.sum(frs[:, 200:], axis=0) == 0)
+    fr_mask = np.logical_or(
+        np.mean(frs[:, 200:650]) * 1000 < 1, np.mean(frs[:, 650:]) * 1000 < 1
+    )
+
+    if np.logical_or(nzeros > 100, fr_mask):
+        return None
+    res["id"] = neu.cluster_group + "_" + str(neu.cluster_number)
+    res["area"] = neu.area
+    res["fr"] = frs
+    return res
+
+
+def sort_neu_by_area(neu1, neu2):
+    idx_sorted = np.argsort([neu1["area"], neu2["area"]])
+    n1_sorted = [neu1, neu2][idx_sorted[0]]
+    n2_sorted = [neu1, neu2][idx_sorted[1]]
+    return n1_sorted, n2_sorted
+
+
+def compute_correlation(popu_fr, n1, n2):
+
+    neu1, neu2 = sort_neu_by_area(popu_fr[n1], popu_fr[n2])
+
+    trial_dur = neu1["fr"].shape[1]
+    # corr = np.corrcoef(neu1['fr'].T,neu2['fr'].T)
+    corr, _ = stats.spearmanr(neu1["fr"], neu2["fr"])
+    if np.all(np.isnan(corr)):
+        # corr = np.array([corr],dtype=np.float16)
+        # p_val = np.array([p_val],dtype=np.float16)
+        return None
+    else:
+        # p_val=p_val[:trial_dur,trial_dur:].astype(np.float16)
+        corr = corr.round(decimals=3)[:trial_dur, trial_dur:].astype(np.float16)
+
+        slices = [slice(0, 200), slice(200, 650), slice(650, None)]
+        mean_corr = np.empty((len(slices), len(slices)))
+        for i in range(len(slices)):
+            for j in range(len(slices)):
+                mean_corr[i, j] = np.nanmean(corr[slices[i], slices[j]])
+
+        corr = mean_corr  # reduce_matrix(corr,2,2,np.nanmean)
+    # corr = np.corrcoef(np.array(t_neus1).T,np.array(t_neus2).T)
+    areas = neu1["area"] + "_" + neu2["area"]
+    return {
+        "areas": areas,
+        "y": neu1["id"],
+        "x": neu2["id"],
+        "corr": corr,
+        "fr_delay_y": np.mean(neu1["fr"][:, 650:]) * 1000,
+        "fr_delay_x": np.mean(neu2["fr"][:, 650:], dtype=np.float16) * 1000,
+        "fr_sample_y": np.mean(neu1["fr"][:, 200:650], dtype=np.float16) * 1000,
+        "fr_sample_x": np.mean(neu2["fr"][:, 200:650], dtype=np.float16) * 1000,
+    }  # ,'p':p_val}
+
+
 # Define parameters
 # Create one population per session
 filepath = "/envau/work/invibe/USERS/IBOS/data/Riesling/TSCM/OpenEphys/new_structure/session_struct/*/neurons/*neu.h5"
+outputpath = "/envau/work/invibe/USERS/IBOS/data/Riesling/TSCM/OpenEphys/correlation/"
 path_list = glob.glob(filepath)
 # Group paths per session
 date_pattern = r"\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}"
@@ -82,99 +149,64 @@ params = [
         "win": 100,
         "dtype_sp": np.int8,
         "dtype_mask": bool,
-    },
-    {
-        "inout": "out",
-        "sp": "sample_on_out",
-        "mask": "mask_out",
-        "event": "sample_on",
-        "time_before": 300,
-        "st": 0,
-        "end": 1550,
-        "select_block": 1,
-        "win": 100,
-        "dtype_sp": np.int8,
-        "dtype_mask": bool,
-    },
+    }
 ]
 # TODO: add parameters to align also to test on
 
-date_time = "2022-12-14_10-50-49"
-datepaths = grouped_paths[date_time]
-population = Parallel(n_jobs=-1)(
-    delayed(get_neu_align)(neu, params) for neu in tqdm(datepaths)
-)
-
-comment = str(params)
-popu = PopulationData(population, comment=comment)
-popu.to_python_hdf5("corr_" + date_time + ".h5")
-
-popu = PopulationData.from_python_hdf5("corr_" + date_time + ".h5")
-
-
-def get_fr(neu, inout, sample, st, end, win=100):
-    res = {}
-    mask = getattr(neu, "mask_" + inout)
-    sp = getattr(neu, "sample_on_" + inout)
-    fr = firing_rate.moving_average(data=sp, win=win, step=1)[:, st:end]
-    sample_id = neu.sample_id[mask]
-    fr_samples = select_trials.get_sp_by_sample(fr, sample_id, [sample])
-    frs = fr_samples[str(sample)]
-    nzeros = np.sum(np.sum(frs[:, 200:], axis=0) == 0)
-    print(nzeros)
-    if np.logical_or(nzeros > 25, np.mean(frs[:, 200:]) * 1000 < 5):
-        return None
-    res["id"] = neu.cluster_group + "_" + str(neu.cluster_number)
-    res["area"] = neu.area
-    res["fr"] = frs
-    return res
-
-
 win = 100
 inout = "in"
-sample = 11
+sample = 0
 st = 100
 end = 1150
 
-popu_fr = popu.execute_function(
-    get_fr, win=win, inout=inout, sample=sample, st=st, end=end, n_jobs=-1, ret_df=False
-)
+for date_time in grouped_paths.keys():
+    print(date_time)
+    datepaths = grouped_paths[date_time]
+    population = Parallel(n_jobs=-1)(
+        delayed(get_neu_align)(neu, params) for neu in tqdm(datepaths)
+    )
 
-fr_dicts_only = [item for item in popu_fr if isinstance(item, dict)]
-len(fr_dicts_only)
+    comment = str(params)
+    popu = PopulationData(population, comment=comment)
+    popu.to_python_hdf5(outputpath + "corr_" + date_time + ".h5")
 
+    for sample in [0, 11, 15, 51, 55]:
+        print(sample)
+        popu = PopulationData.from_python_hdf5(outputpath + "corr_" + date_time + ".h5")
 
-def compute_correlation(popu_fr, n1, n2):
-    neu1 = popu_fr[n1]
-    neu2 = popu_fr[n2]
-    if np.logical_or(neu1 is None, neu2 is None):
-        return None
-    trial_dur = neu1["fr"].shape[1]
-    corr = np.corrcoef(neu1["fr"].T, neu2["fr"].T)
-    # corr,p_val = stats.spearmanr(neu1['fr'],neu2['fr'])
-    if np.all(np.isnan(corr)):
-        corr = np.array([corr], dtype=np.float16)
-        # p_val = np.array([p_val],dtype=np.float16)
-    else:
-        # p_val=p_val[:trial_dur,trial_dur:].astype(np.float16)
-        corr = corr.round(decimals=3)[:trial_dur, trial_dur:].astype(np.float16)
-    # corr = np.corrcoef(np.array(t_neus1).T,np.array(t_neus2).T)
-    areas = neu1["area"] + "_" + neu2["area"]
-    return {
-        "areas": areas,
-        "y": neu1["id"],
-        "x": neu2["id"],
-        "corr": corr,
-    }  # ,'p':p_val}
+        popu_fr = popu.execute_function(
+            get_fr,
+            win=win,
+            inout=inout,
+            sample=sample,
+            st=st,
+            end=end,
+            n_jobs=-1,
+            ret_df=False,
+        )
 
+        fr_dicts_only = [item for item in popu_fr if isinstance(item, dict)]
 
-numbers = range(len(fr_dicts_only))
-pairs = list(itertools.combinations(numbers, 2))  # Compute each pair in parallel
-len(pairs)
+        if len(fr_dicts_only) == 0:
+            continue
 
-res = Parallel(n_jobs=-1)(
-    delayed(compute_correlation)(fr_dicts_only, n1, n2) for n1, n2 in tqdm(pairs)
-)
-res_dicts_only = [item for item in res if isinstance(item, dict)]
-with open("corr_" + date_time + ".pkl", "wb") as fp:
-    pickle.dump(res_dicts_only, fp)
+        numbers = range(len(fr_dicts_only))
+        pairs = list(
+            itertools.combinations(numbers, 2)
+        )  # Compute each pair in parallel
+        len(pairs)
+
+        res = Parallel(n_jobs=-1)(
+            delayed(compute_correlation)(fr_dicts_only, n1, n2)
+            for n1, n2 in tqdm(pairs)
+        )
+        res_dicts_only = [item for item in res if isinstance(item, dict)]
+
+        if len(res_dicts_only) == 0:
+
+            continue
+
+        with open(
+            outputpath + "corr_s" + str(sample) + "_" + date_time + ".pkl", "wb"
+        ) as fp:
+            pickle.dump(res_dicts_only, fp)
