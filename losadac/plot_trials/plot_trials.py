d19172a9248722146f5872811ba9b1856151750a
camila <camilosadaa@hotmail.com>
2024-09-22 19:18:57 +0200

    wip

--- losadac/plot_trials/plot_trials.py
+++ losadac/plot_trials/plot_trials.py
@@ -196,7 +196,8 @@ def plot_trials(
 ):
 
     neu = NeuronData.from_python_hdf5(neupath)
-    nid = neu.get_neuron_id()
+    # nid = neu.get_neuron_id()
+    nid = f"{neu.subject}_{neu.area.upper()}_{neu.date_time}_{neu.cluster_group}{int(neu.cluster_number)}"
     print(nid)
     sp, conv = prepare_data_plotb1(
         neu,
