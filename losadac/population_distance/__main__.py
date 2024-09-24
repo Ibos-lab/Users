da044e1b39ebcdc0a8ce44ce15864220b7fb9fc9
Camila Losada <camilosadaa@hotmail.com>
2024-09-24 14:46:24 +0200

    wip

--- losadac/population_distance/__main__.py
+++ losadac/population_distance/__main__.py
@@ -6,7 +6,7 @@ from . import compute_population_distance
 
 def main(preprocessing: Dict, paths: Dict, **kwargs):
 
-    res = compute_population_distance.compute_distance(preprocessing, paths)
+    res = compute_population_distance.compute_distance(**preprocessing, **paths)
     print("saving")
     res.to_python_hdf5("population_distance.h5")
 
