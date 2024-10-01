dee162080b74910ce69348e1de813ad817805928
Camila Losada <camilosadaa@hotmail.com>
2024-10-01 16:08:24 +0200

    ~

--- losadac/plot_trials/plot_trials.py
+++ losadac/plot_trials/plot_trials.py
@@ -210,7 +210,7 @@ def plot_trials(
 
     fig = neu.plot_sp_b1(sp, conv)
     fig.savefig(
-        f"./{neu.area}/{nid}.{format}",
+        f"./{nid}.{format}",
         format=format,
         bbox_inches="tight",
         transparent=False,
